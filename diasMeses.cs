6481f992639b4f5169d0225cfa0d058da6350471
Daniel Franco <82846096+Franccodan@users.noreply.github.com>
2022-03-22 22:20:35 -0300

    Add files via upload

---
+++ diasMeses.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text;
+
+
+namespace diasMeses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome = "";
+            int dias = 0;
+            const int diasMeses = 30;
+            int resultado = 0;
+
+
+            Console.Write("Digite o seu nome: ");
+            nome = Console.ReadLine();
+
+            Console.Write("Digite a quantidade de dias: ");
+            dias = int.Parse(Console.ReadLine());
+
+
+            resultado = dias / diasMeses;
+
+            Console.Write(nome +  dias + " Dias são " + resultado + " meses ");
+        
+
+            Console.ReadLine();
+
+
+
+
+
+
+        }
+    }
+}
